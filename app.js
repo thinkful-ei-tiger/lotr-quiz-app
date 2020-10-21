bf9f528bc6763be22cd7fac39906dd334bf0882b
Rob Blaine <rgblaine70@gmail.com>
2020-10-21 10:11:17 -0600

    added answer page and handleAnswer function

--- app.js
+++ app.js
@@ -4,117 +4,120 @@
 'use strict';
 
 const store = {
-    // 5 or more questions are required
-    questions: [
-        {
-            question: 'Which one is NOT a hobbit?',
-            answers: [
-                'Frodo',
-                'Bilbo',
-                'Sam',
-                'Sleepy'
-            ],
-            correctAnswer: 'Sleepy'
-        },
-        {
-            question: 'What was the name of the dark lord?',
-            answers: [
-                'Doctor Evil',
-                'Sauron',
-                'Thanos',
-                'Darth Vader'
-            ],
-            correctAnswer: 'Sauron'
-        },
-        {
-            question: 'What was the name Gandalf’s sword?',
-            answers: [
-                'Orcrist',
-                'Sting',
-                'Glamdring',
-                'Narsil'
-            ],
-            correctAnswer: 'Glamdring'
-        },
-        {
-            question: 'What was the name of Sam’s Pony?',
-            answers: [
-                'Bill',
-                'Rocket',
-                'Lightning',
-                'Bailey'
-            ],
-            correctAnswer: 'Bill'
-        },
-        {
-            question: 'Which is not an alias of the balgrog that Gandalf fights in the Mines of Moria?',
-            answers: [
-                'The Nameless Terror',
-                'Durin\'s Bane',
-                'Flame of Udun',
-                'Morgoth\'s Might'
-            ],
-            correctAnswer: 'Morgoth\'s Might'
-        },
-        {
-            question: 'What is the name of Morgoth\'s signature warhammer?',
-            answers: [
-                'Grond',
-                'Thrangell',
-                'Gurthang',
-                'The Gopher\'s Nightmare'
-            ],
-            correctAnswer: 'Grond'
-        },
-        {
-            question: 'What races were Beren and Luthien, respectively?',
-            answers: [
-                'dwarf and elf',
-                'elf and elf',
-                'human and elf',
-                'chipmunk and squarrel'
-            ],
-            correctAnswer: 'human and elf'
-        }
-    ],
-    quizStarted: false,
-    questionNumber: 0,
-    score: 0,
-    currentPage: '',
-    currentAnswer: ''
+  // 5 or more questions are required
+  questions: [
+    {
+      question: 'Which one is NOT a hobbit?',
+      answers: [
+        'Frodo',
+        'Bilbo',
+        'Sam',
+        'Sleepy'
+      ],
+      correctAnswer: 'Sleepy'
+    },
+    {
+      question: 'What was the name of the dark lord?',
+      answers: [
+        'Doctor Evil',
+        'Sauron',
+        'Thanos',
+        'Darth Vader'
+      ],
+      correctAnswer: 'Sauron'
+    },
+    {
+      question: 'What was the name Gandalf’s sword?',
+      answers: [
+        'Orcrist',
+        'Sting',
+        'Glamdring',
+        'Narsil'
+      ],
+      correctAnswer: 'Glamdring'
+    },
+    {
+      question: 'What was the name of Sam’s Pony?',
+      answers: [
+        'Bill',
+        'Rocket',
+        'Lightning',
+        'Bailey'
+      ],
+      correctAnswer: 'Bill'
+    },
+    {
+      question: 'Which is not an alias of the balgrog that Gandalf fights in the Mines of Moria?',
+      answers: [
+        'The Nameless Terror',
+        'Durin\'s Bane',
+        'Flame of Udun',
+        'Morgoth\'s Might'
+      ],
+      correctAnswer: 'Morgoth\'s Might'
+    },
+    {
+      question: 'What is the name of Morgoth\'s signature warhammer?',
+      answers: [
+        'Grond',
+        'Thrangell',
+        'Gurthang',
+        'The Gopher\'s Nightmare'
+      ],
+      correctAnswer: 'Grond'
+    },
+    {
+      question: 'What races were Beren and Luthien, respectively?',
+      answers: [
+        'dwarf and elf',
+        'elf and elf',
+        'human and elf',
+        'chipmunk and squarrel'
+      ],
+      correctAnswer: 'human and elf'
+    }
+  ],
+  quizStarted: false,
+  questionNumber: 0,
+  score: 0,
+  currentPage: '',
+  currentAnswer: ''
 };
 
 
-function handleNextButton() {
-  $('main').on('click', '#next-button', function (event) {
-      if (store.questionNumber === store.questions.length - 1) {
-          store.currentPage = 'final'
-      } else {
-          store.questionNumber++;
-          store.currentPage = 'question'
-      }
-      render();
-  });
-}
-
-
 function answerPage() {
+  let answerTemplate = '';
   let correctAnswer = store.questions[store.questionNumber].correctAnswer;
   if (store.currentAnswer === correctAnswer) {
-      store.score++
-      answerTemplate = '<p>Correct!</p>'
+    store.score++
+    answerTemplate = '<p>Correct!</p>'
   } else {
-      answerTemplate = `<p>Sorry, wrong answer. The correct answer is ${correctAnswer}</p>`
+    answerTemplate = `<p>Sorry, wrong answer. The correct answer is ${correctAnswer}</p>`
   }
 
   return `<section>
-      <h2 class='.score'>Your Score: ${store.score} Correct / ${store.questionNumber - store.score} Incorrect</h2>
+      <h2 class='.score'>Your Score: ${store.score} Correct / ${store.questionNumber - store.score + 1} Incorrect</h2>
+      <p class='.question-number'>Question Number: ${store.questionNumber + 1} of ${store.questions.length}</p>
       ${answerTemplate}
       <form action =''>
           <button id='next-button' name='next-button' type='submit'>Next</button>
       </form>
  </section> `;
 }
+
+
+function handleNextButton() {
+  $('main').on('click', '#next-button', function (event) {
+    store.questionNumber++;
+    if (store.questionNumber >= store.questions.length) {
+      store.currentPage = 'final'
+    } else {
+      store.currentPage = 'question'
+    }
+    render();
+  });
+}
+
 
 function startPage() {
   return `<section>
@@ -128,18 +131,19 @@ function startPage() {
 
 
 
-function createAnswerList(){
+function createAnswerList() {
   let answerList = '';
   let answers = store.questions[store.questionNumber].answers;
-  answers.forEach(element => { 
+  answers.forEach(element => {
     answerList += `<label for='submit-answer'>${element}</label> 
     <input type='radio' name="answers" value=${element}>`;
-  }); 
+  });
   return answerList;
 }
 
 function handleSubmitAnswer() {
-  $('main').on('click', '#submit-answer', function(event) {
+  $('main').on('click', '#submit-answer', function (event) {
+    event.preventDefault();
     console.log('Answer has been submitted');
     store.currentPage = 'answer';
     store.currentAnswer = $(`input[name='answers']:checked`).val();
@@ -162,16 +166,13 @@ function questionPage() {
 </section>`;
 }
 
-function answerPage() {
-  return ``;
-}
 
 function finalPage() {
-  return ``;
+  return `<p>Final Page</p>`;
 }
 
 function handleStartQuiz() {
-  $('main').on('click', '#start', function(event) {
+  $('main').on('click', '#start', function (event) {
     console.log('One does not simply click the start button.')
     console.log(`quizStarted`, store.quizStarted);
     store.quizStarted = true;
@@ -182,10 +183,10 @@ function handleStartQuiz() {
 }
 
 function render() {
-  if (store.quizStarted === false) { 
+  if (store.quizStarted === false) {
     $('main').html(startPage());
     console.log('displaying startPage');
-  } else if (store.quizStarted) { 
+  } else if (store.quizStarted) {
     if (store.currentPage === 'question') {
       $('main').html(questionPage());
       console.log('displaying questionPage');
@@ -194,22 +195,23 @@ function render() {
     } else {
       $('main').html(finalPage());
     }
-  } 
+  }
 }
 
 function main() {
   render();
   handleStartQuiz();
   handleSubmitAnswer();
+  handleNextButton();
 }
 
 $(main());
 
 /**
- * 
+ *
  * Technical requirements:
- * 
- * Your app should include a render() function, that regenerates the view each time the store is updated. 
+ *
+ * Your app should include a render() function, that regenerates the view each time the store is updated.
  * See your course material, consult your instructor, and reference the slides for more details.
  *
  * NO additional HTML elements should be added to the index.html file.
@@ -217,7 +219,7 @@ $(main());
  * You may add attributes (classes, ids, etc) to the existing HTML elements, or link stylesheets or additional scripts if necessary
  *
  * SEE BELOW FOR THE CATEGORIES OF THE TYPES OF FUNCTIONS YOU WILL BE CREATING 👇
- * 
+ *
  */
 
 /********** TEMPLATE GENERATION FUNCTIONS **********/
